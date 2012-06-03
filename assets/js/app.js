17d84cc1c6016e1e0d9f225ca8672a733c5ffdd1
Jack Thorsen <jdt@mind.net>
2012-06-03 19:30:02 -0300

    Update master

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,8 +28,8 @@ run(function () {
     		     x$('#documents').inner(stuffing);
     		     var el=x$('.row_button');
     		     x$('.row_button').on('touchstart',function () {
-    		     	var tu="http://dev.budgetblogs.com:3000/citi_docs/";
-    		     	x$('#looking').bottom(tu);
+    		     	var tu="http://dev.budgetblogs.com:3000/citi_docs/" + this.id;
+    		     	x$('#looking').xhr('inner', tu);
     		     	alert("Why can't this work?" + this.id);
     		     	display('#looking');
     		     });
