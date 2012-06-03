a43e1fccf8373da007c151f7eaebebb1d781414c
Jack Thorsen <jdt@mind.net>
2012-06-03 18:56:55 -0300

    Update master

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ run(function () {
   		     stuffing += "</table></div><p>";
     		     x$('#documents').inner(stuffing);
     		     var el=x$('.row_button');
-    		     x$('.row_button').touchstart(alert("hello" + this.id));
+    //		     x$('.row_button').touchstart(alert("hello" + this.id));
     //		     var tc="<p>" + el.length;
    // 		     x$('#looking').html(tc);
  //   		     display('#looking');
