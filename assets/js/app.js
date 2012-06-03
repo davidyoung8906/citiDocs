a2935571b414b5cd6e9cd56a77c3050482207058
Jack Thorsen <jdt@mind.net>
2012-06-03 01:59:35 -0300

    Update assets/js/app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,8 @@ run(function () {
         if (navigator.network.connection.type == Connection.NONE) {
             alert("No internet connection - cannot access remote documents");
         } else {
- //       	var cityDocs = x$('#documents').xhr('http://dev.budgetblogs.com:3000/city_docs.json');
-   //     	var stuffing = "City Documents<table>" + citiDocs.length;
+       	var cityDocs = x$('#documents').xhr('http://dev.budgetblogs.com:3000/city_docs.json');
+     	var stuffing = "City Documents<table>" + citiDocs.length;
     //		for (var i=0; i<cityDocs.length; i++){
     //			row = cityDocs[i];
     //			stuffing += "<tr><td>id: " + row.id + "</td><td>title:" + row.title + "</td><td>type:" + row.type + "</td></tr>"; 
