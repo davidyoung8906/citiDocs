43b737298f048d1613e3dd07722ab3ad600d42b8
Jack Thorsen <jdt@mind.net>
2012-06-03 18:03:35 -0300

    Update master

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,7 +32,7 @@ run(function () {
     		     display('#looking');
     		     for (var i=0; i<el.length; i++) {
     		     	var e=el[i];
-    		     	e.on('firsttouch', function () {
+    		     	e.on('touchstart', function () {
   		     		var ls="http://dev.budgetblogs.com:3000/city_docs/" + e.id;
   		     		x$('#looking').xhr(ls);
   		     		display('#looking');
