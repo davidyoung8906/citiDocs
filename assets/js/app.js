89ea26acaf06c64139a5aabbedd883d21b922f39
Jack Thorsen <jdt@mind.net>
2012-06-03 17:30:53 -0300

    Update master

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,7 @@ run(function () {
     		     x$('#documents').inner(stuffing);
     		     var el=x$('.row_button');
     		     x$('#looking').inner(el);
+    		     display('#looking');
  //   		     for (i=0; el.length; i++) {
 //    		     	var e=el[i];
   //  		     	e.on('firsttouch', function () {
