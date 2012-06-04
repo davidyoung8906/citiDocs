fea87a5195717e1ed047c3fb6e50e00e69afd8cd
Jack Thorsen <jdt@mind.net>
2012-06-04 01:15:22 -0300

    Update master

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ run(function () {
                 callback: function(){
                      
                      var cities = eval("("+this.responseText+")"); /* this should be an array */
-                     var stuffing = "<div id='docContent'><h1>City Documents</h1><p>Total Number of Cities: " + cities.length + " <table> ";
+                     var stuffing = "<div id='docContent'><h1>City Documents</h1><p>Total Number of Documents: " + cities.length + " <table> ";
    		     for (var i=0; i<cities.length; i++){
   			var row = cities[i];
   			stuffing += "<tr><td>" 
