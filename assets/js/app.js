53bdb8a616ae6af3fbd05558a2694465173e58fb
Jack Thorsen <jdt@mind.net>
2012-06-17 15:16:39 -0300

    Update master

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,10 +12,10 @@ run(function () {
        	     x$('#documents').xhr('http://dev.budgetblogs.com:3000/page/pagelist.json',{
                 callback: function(){
                      
-                     var list = eval("("+this.responseText+")"); /* this should be an array or hash */
+                     var listing = eval("("+this.responseText+")"); /* this should be an array or hash */
                      var stuffing = "<div id='docContent'><h1>City Documents</h1><p>Total Number of Documents for "
                      			+ "Portsmouth NH " + " is " 
-                     			+ list.length + " <table> ";
+                     			+ listing.length + " <table> ";
                      val i=1;
  //  		     for (var k in list) {
   //			stuffing += "<tr class='trow' id="
