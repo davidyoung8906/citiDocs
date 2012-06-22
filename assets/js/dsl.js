0f62b0c7ef62f1496e05b345b5629a0768a49d2d
Jack Thorsen <jdt@mind.net>
2012-06-22 00:04:57 -0300

    Update master

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -56,7 +56,7 @@ var run = function(application) {
 , listings = function() {
 	var listing = getfilestore();
 	x$("#welcome").bottom("<p>back to listings with list");
-	x$("#welcome").bottom(listing);
+	x$("#welcome").bottom("Listing:  " + listing);
 //    	displayit(listing);
 //	if (navigator.network.connection.type == Connection.NONE) {
 //            alert("No internet connection - cannot access remote documents");
