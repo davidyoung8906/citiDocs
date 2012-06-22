fb998b0014c7ced1b8b5263ed7a3f121e14fd764
Jack Thorsen <jdt@mind.net>
2012-06-22 02:14:04 -0300

    Update master

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -43,17 +43,17 @@ var run = function(application) {
 }
 
 , displayit = function(listing) {
-	var stuffing = "<div id='docContent'><h1>City Documents</h1><p>Total Number of Documents for "
-       			+ "Portsmouth NH " + " is " 
-       			+ Object.keys(listing).length + " <table> ";
+//	var stuffing = "<div id='docContent'><h1>City Documents</h1><p>Total Number of Documents for "
+//       			+ "Portsmouth NH " + " is " 
+//       			+ Object.keys(listing).length + " <table> ";
 //	for (var k in listing) {
 //		stuffing += "<tr class='trow' id="
 //				+ k + "><td>" 
 //				+ k + "</td><td>"
 //				+ listing[k] + "</td></tr>"; 			
  //	};
- 	stuffing += "</table></div><p>";
- 	x$('#documents').inner(stuffing);
+// 	stuffing += "</table></div><p>";
+// 	x$('#documents').inner(stuffing);
  //	x$('.trow').on('click',function () {
    //  		var tv='<iframe width="640" height="360" src="'
      //			+ this.id + '" frameborder="0" allowfullscreen></iframe>';
