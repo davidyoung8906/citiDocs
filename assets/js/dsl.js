3011ffece949f47b4a995721c5a8af85c196c0bd
Jack Thorsen <jdt@mind.net>
2012-06-23 20:28:12 -0300

    Update master

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -100,8 +100,7 @@ var run = function(application) {
 , docsubs = function(subs, focus) {  // subs is an array of subs like ["sub1", "sub2",...]
 		x$("#docsubs").html("<span id='subs'>");
 		for (var i=0; i<subs.length; i++) {
-			var subbutton = "<button "
-					+ "onclick='pushcrumb(" + focus + ")'>"
+			var subbutton = "<button onclick='pushcrumb(" + focus + ")'>"
 					+ crumbs[i] + "</button>";
 			x$("#welcome").bottom("sub factory: " + i);
 			x$("#docsubs").bottom("" + subbutton);	
