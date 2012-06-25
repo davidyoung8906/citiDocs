5f295cd4566d58c8c7220b8b8bfb16e7408b072a
Jack Thorsen <jdt@mind.net>
2012-06-25 16:32:23 -0300

    Update master

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -90,8 +90,8 @@ var run = function(application) {
 		for (k in docs[i]) {
 			stuffing += "<tr class='trow' id='"
 				+ k + "'><td>" 
-				+ "<button class='docurl' src='" + docpath + k + "'>" + k + "</a></td><td>"
-				+ "<button class='doctitle' src='" + docpath + k + "'>" + docs[i][k] + "</a></td></tr>";
+				+ "<button class='docurl' src='" + docpath + k + "'>" + k + "</button></td><td>"
+				+ "<button class='doctitle' src='" + docpath + k + "'>" + docs[i][k] + "</button></td></tr>";
 			x$("#welcome").bottom("full path: " + docpath + k);	
 		};
  	};
