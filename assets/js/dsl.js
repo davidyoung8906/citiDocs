ae9ef362859e824f52fcc7ec34287e25dc621353
Jack Thorsen <jdt@mind.net>
2012-06-24 23:18:27 -0300

    Update master

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -117,7 +117,7 @@ var run = function(application) {
 			var ref = "#subbutton" + i;
 			var newfocus = '"' + subs[i] + '"';
 			x$("#welcome").bottom("on click: " + i + " " + newfocus);
-			x$(ref).on('click', function(e, newfocus) {resolvedown(newfocus);}); 
+			x$(ref).on('click', {f:newfocus}, function() {resolvedown(data.f);}); 
 		}
 }
 
