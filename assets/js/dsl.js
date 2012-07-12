d0e24dba6f3b15693b5b583937ca3f377c307414
Jack Thorsen <jdt@mind.net>
2012-07-12 12:59:01 -0700

    Update master

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -93,7 +93,7 @@ var run = function(application) {
 						+ crumbs[i] + "</li>";
 			$("#crumblist").append("" + crumbbutton);	
 		};
-//		$('#crumblist').trigger('create');
+		$('#crumblist').listview('refresh');
 }
 
 , popcrumbs = function(n) {
