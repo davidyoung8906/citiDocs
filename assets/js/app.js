6ee5f9de0117555838170f1d642791e3784f188f
Jack Thorsen <jdt@mind.net>
2012-07-13 21:04:34 -0700

    More fulllist

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ run(function () {
     // immediately invoked on first run
     var init = (function () {
     	listings();
-        
+        fulllist();
         store.get('city', function(saved) {
     		if (saved) {if (saved.value) {x$('input#city_input').attr('placeholder', saved.value);};}    		
     	});
