357bdd2e6e696843e250e394e85f8f12e67ab6ca
Jack Thorsen <jdt@mind.net>
2012-07-15 22:47:50 -0700

    dd

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -97,7 +97,7 @@ var run = function(application) {
 }
 
 , ulinput = function() {
-	$(".ui-header h1").each(function () {this.prepend("<a data-rel='back' data-icon='arrow-l'>BACK</a>");});
+	$(".ui-header").each(function () {this.prepend("<a data-rel='back' data-icon='arrow-l'>BACK</a>");});
 }
 
 //  {"http://":[{"censta                Report (click here to view)"},{"agendas":[{"2001":[{"bfcm120501.pdf":"Minutes"},{"boa":[{"BOALN121801.pdf":"Notice"},{"boa071701ln.pdf":"Notice"},{"boa082101ln.pdf":"Notice"},{"boa091801ag.pdf":"Agenda"},{"boa091801ln.pdf
