6240827d7b0c38cda19af29b3289467b600b3acf
Jack Thorsen <jdt@mind.net>
2012-07-18 18:55:41 -0300

    Update assets/js/dsl.js

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -93,7 +93,7 @@ var run = function(application) {
 		s=saved.value;
 	});
 //	$("#fulllist").append("<li>got response</li>");
-	return s.replace(/{/g, '<li>').replace(/}/g,'</li>').replace(/(\,)/g,'').replace(/:\[/g,'<ul data-role="listview" data-inset="true">').replace(/\]/g,'</ul>').replace(/"(.(?!")*.":(?!\[)"(.(?!"))*."/g, '<li><a href="$1" >$2</a></li>');
+	return s.replace(/{/g, '<li>').replace(/}/g,'</li>').replace(/(\,)/g,'').replace(/\]/g,'</ul>').replace(/("(?:.(?!")*."):(?!\[)("(?:.(?!"))*.")/g, '<a href="$1" >$2</a>').replace(/:\[/g,'<ul data-role="listview" data-inset="true">');
 //	$("#fulllist").append("<li>end of makelist</li>");
 }
 
