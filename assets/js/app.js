f43ec868aae441b2e00678356f36cb0af63c421d
Jack Thorsen <jdt@mind.net>
2012-07-19 00:25:36 -0300

    Update assets/js/app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,6 @@ run(function () {
     var init = (function () {
     	$(':jqmData(url^="home")').live('pagebeforecreate', function(event) {
     		$(this).filter(':jqmData(url*=ui-page)').find(':jqmData(role=header)')
-    			.prepend('<a href="#home" data-icon="gear" data-iconpos="right">Home</a>')
     			.prepend('<a href="#home" data-icon="home" data-iconpos="right">Home</a>')
       			.prepend('<a href="#" data-rel="back" data-icon="back">Back</a>')
       			.attr('data-position','fixed')
