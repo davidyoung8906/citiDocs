ab0588c70b9343dbbb4ee90c0a1f17131cdba3ac
Jack Thorsen <jdt@mind.net>
2012-07-19 23:59:16 -0300

    Update assets/js/dsl.js

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -132,7 +132,7 @@ var run = function(application) {
 
 
 , doccrumbs = function() {
-		$("#crumblist").html("<li data-role='list-divider' data-theme='e'>Current Document Folder: ");
+		$("#crumblist").html("<li data-role='list-divider' data-theme='e'>");
 		var crumbs = getcrumbs();
 		for (var i=0; i<crumbs.length; i++) {
 			var crumbbutton = "<a class='ui-aside' data-inline='true' onclick='popcrumbs(" 
