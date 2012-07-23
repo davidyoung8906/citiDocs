dab6f00816627f87ff86f14e94f17b774b8aaede
Jack Thorsen <jdt@mind.net>
2012-07-23 13:00:37 -0400

    xx

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,13 +13,14 @@ run(function () {
 //      			.attr('data-position','fixed')
 //  	});
  // 	$.mobile.loading( 'show', { theme: "b", text: "Loading Data Files" });
-    	listings();
-        fulllist();
-        store.get('city', function(saved) {
-    		if (saved) {if (saved.value) {x$('#city_input').attr('placeholder', saved.value);};}    		
-    	});
     	x$("input#city_input").on("keyup", function() {
     		getcitylist(this.value);	
     	});
+    	listings();
+        fulllist();
+        store.get('city', function(saved) {
+    		if (saved) {if (saved.value) {x$('input#city_input').attr('placeholder', saved.value);};}    		
+    	});
+    	
     })();
 });
