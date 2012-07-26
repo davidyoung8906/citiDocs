c921e7f298cbf9748db825b4734aca27205917ee
Jack Thorsen <jdt@mind.net>
2012-07-26 12:26:42 -0400

    11

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -23,7 +23,7 @@ var run = function(application) {
 	} else {
 		$.get('http://dev.budgetblogs.com:3000/page/pagelist.json',{chars:part}, function(data) {
 //				$("#logs").append("here's the raw data :" + data);
-				$("#citylist").fadeTo('fast',0.5);
+//				$("#citylist").fadeTo('fast',0.5);
 				citylist(data);
 		});
 	};
@@ -44,7 +44,7 @@ var run = function(application) {
 //		$("#logs").append("<li>city list has: " + cities + "</li>");
 	};
 	$("#citylist").listview("refresh");
-	$("#citylist").fadeTo('fast',1);
+//	$("#citylist").fadeTo('fast',1);
 }
 
 , getfile = function() {
