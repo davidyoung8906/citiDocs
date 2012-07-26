6254008aa42c5ea5c7f23b5665dae1b0ce7ca163
Jack Thorsen <jdt@mind.net>
2012-07-26 17:58:01 -0400

    m

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,9 +15,11 @@ run(function () {
  // 	$.mobile.loading( 'show', { theme: "b", text: "Loading Data Files" });
     	
 //    	$("#logs").append("init");
+		$("#city_search").focus();
 //		$("#city_search").on("keyup", function() {
 //    					getcitylist(this.value);	
 //    				});
+		
     	listings();
         fulllist();
         store.get('city', function(saved) {
